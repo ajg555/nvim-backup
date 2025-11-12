cf6d2609ae9f2856a4d1b2da3c63f36337d21c5f
ajg555 <alexjg042@gmail.com>
2025-11-12 02:56:16 -0300

    rev: correcao do script de reconfiguracao de backup do neovim

--- nvim_backup.sh
+++ nvim_backup.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
-# vim-plug: https://github.com/junegunn/vim-plug
+# WARNING: This script overwrites the current configuration
+
+# vim-plug install : https://github.com/junegunn/vim-plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-echo "" > ~/.config/nvim/init.vim  
-cat init.vim > ~/.config/nvim/init.vim
-nvim +:PlugInstall
+# creating/overwriting configuration files
+SETTINGS_DIR="$HOME/.config/nvim"
+SETTINGS_FILE="$SETTINGS_DIR/init.vim"
+
+if [ ! -f "$SETTINGS_FILE" ];
+	then
+		mkdir -p $SETTINGS_DIR \
+		&& touch $SETTINGS_FILE
+fi		
+
+cat init.vim > $SETTINGS_FILE
+
+# install plugins with vim-plug
+nvim source % +:PlugInstall
